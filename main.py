8d7b2115d3b5125f64cb2c71fc331a9d42611855
wneto2020 <69701692+wneto2020@users.noreply.github.com>
2021-04-08 01:36:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,32 +16,40 @@ def sheet_import(planilha):
 def sheet_finally():
     gc = gspread.service_account(filename='service_account.json')
     sh = gc.open('list_final')
-    return sh.sheet1.get_all_records()
+    return sh.sheet1.get_all_values()
 
-def write_sheet(x):
+
+def write_sheet(x:list):
     gc = gspread.service_account(filename='service_account.json')
     sh = gc.open('list_final')
     work = sh.sheet1
-    work.append_row()
+    work.append_row(x)
+
+sheet_request = sheet_request()
+sheet_import = sheet_import(planilha)
+sheet_finally = sheet_finally()
+
+def transfer_data():
+
+    for res in sheet_request:
+        for res2 in sheet_import:
+            if res['Email'] == res2['Email']:
+                list_insert.append(res)
+
+    lenght = len(sheet_finally)
+
+    for item in list_insert:
+        i = 0
+        for res in sheet_finally:
+            if item['Email'] in res:
+                print('Já existente')
+            else:
+                i += 1
+
+        if i == lenght:
+            write_sheet([item['Nome'], item['Email'], item['cpf'], item['telefone']])
 
 
 
-
-sheet_req = sheet_request()
-
-sheet_impor = sheet_import(planilha)
-sheet_final = sheet_finally()
-
-for res in sheet_req:
-    for res2 in sheet_impor:
-        if res['cpf'] == res2['cpf'] and res['Email'] == res2['Email']:
-            list_insert.append(res)
-
-i = 0
-for item in list_insert:
-    for res in sheet_final:
-        if item['cpf'] != sheet_final['cpf'] and item['Email'] != sheet_final['Email']:
-
-
-
-
+transfer_data()
+print(sheet_finally)
